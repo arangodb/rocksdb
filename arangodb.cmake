23f581c41c9e43bc16410a8453efe889a99b3bdd
jsteemann <jsteemann@users.noreply.github.com>
2023-04-25 11:47:52 +0200

    make it compile again

--- arangodb.cmake
+++ arangodb.cmake
@@ -7,9 +7,6 @@ if (CMAKE_CXX_COMPILER_ID STREQUAL "Clang")
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-suggest-override")
 endif()
 
-# we want the following definitions to be in effect for both rocksdb and arangodb
-add_definitions("-DNROCKSDB_THREAD_STATUS")
-
 # IPO_ENABLED is set by the top-level CMakeLists.txt file
 if (IPO_ENABLED)
   set(CMAKE_INTERPROCEDURAL_OPTIMIZATION True)
