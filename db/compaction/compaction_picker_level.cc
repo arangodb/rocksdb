cc714a8bab2a6c339255d91c7ba93da0162953d5
jsteemann <jsteemann@users.noreply.github.com>
2023-09-25 10:19:27 +0200

    change back to upstream after they merged a fix

--- db/compaction/compaction_picker_level.cc
+++ db/compaction/compaction_picker_level.cc
@@ -779,17 +779,9 @@ bool LevelCompactionBuilder::PickFileToCompact() {
   const std::vector<int>& file_scores =
       vstorage_->FilesByCompactionPri(start_level_);
 
-  unsigned int cmp_idx = vstorage_->NextCompactionIndex(start_level_);
-  for (std::size_t i = 0; i < file_scores.size(); i++, cmp_idx++) {
-    if (cmp_idx >= file_scores.size()) {
-      // we might be starting in the middle or even the end of the file list,
-      // but we might need to reconsider some of the files we have previously
-      // skipped, so we need to wraparound. See
-      // https://github.com/facebook/rocksdb/issues/10257#issuecomment-1180534048
-      // for details why this is necessary.
-      cmp_idx = 0;
-    }
-
+  unsigned int cmp_idx;
+  for (cmp_idx = vstorage_->NextCompactionIndex(start_level_);
+       cmp_idx < file_scores.size(); cmp_idx++) {
     int index = file_scores[cmp_idx];
     auto* f = level_files[index];
 
