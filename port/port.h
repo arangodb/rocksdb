08c207af1447a92e60879c13b899868d5e0e9332
Frank Celler <frank@arangodb.com>
2025-05-22 08:59:10 +0200

    windows

--- port/port.h
+++ port/port.h
@@ -14,8 +14,8 @@
 // Include the appropriate platform specific file below.  If you are
 // porting to a new platform, see "port_example.h" for documentation
 // of what the new port_<platform>.h file must provide.
-#if defined(ROCKSDB_PLATFORM_POSIX)
-#include "port/port_posix.h"
-#elif defined(OS_WIN)
+#if defined(OS_WIN)
 #include "port/win/port_win.h"
+#elif defined(ROCKSDB_PLATFORM_POSIX)
+#include "port/port_posix.h"
 #endif
